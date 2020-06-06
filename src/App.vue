f402aefa057cba924b93bb31bcec4ddecc1e9ac6
oguzhan.celikkaya@itelligence.com.tr <oguzhancelikkaya@gmail.com>
2020-06-06 23:45:28 +0300

    transitions

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 <template>
   <div>
     <appHeader></appHeader>
-    <router-view></router-view>
+    <transition name="slide" mode="out-in">
+      <router-view></router-view>
+    </transition>  
     <appFooter></appFooter>
   </div> 
 </template>
@@ -19,5 +21,36 @@ export default {
 </script>
 
 <style lang="scss">
+    .slide-leave-active {
+        transition: opacity .3s ease;
+        opacity: 0;
+        animation: slide-out .3s ease-out forwards;
+    }
 
+    .slide-leave {
+        opacity: 1;
+        transform: translateX(0);
+    }
+
+    .slide-enter-active {
+        animation: slide-in .3s ease-out forwards;
+    }
+
+    @keyframes slide-out {
+        0% {
+            transform: translateY(0);
+        }
+        100% {
+            transform: translateY(-10px);
+        }
+    }
+
+    @keyframes slide-in {
+        0% {
+            transform: translateY(-10px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
 </style>
--- src/view/Movies/Movies.vue
+++ src/view/Movies/Movies.vue
@@ -1,9 +1,42 @@
 <template>
     <div class="container">
-       <router-view></router-view> 
+        <transition name="slide" mode="out-in">
+            <router-view></router-view> 
+        </transition>
     </div>
 </template>
 
-<script>
+<style>
+    .slide-leave-active {
+        transition: opacity .3s ease;
+        opacity: 0;
+        animation: slide-out .3s ease-out forwards;
+    }
 
-</script>
+    .slide-leave {
+        opacity: 1;
+        transform: translateX(0);
+    }
+
+    .slide-enter-active {
+        animation: slide-in .3s ease-out forwards;
+    }
+
+    @keyframes slide-out {
+        0% {
+            transform: translateY(0);
+        }
+        100% {
+            transform: translateY(-10px);
+        }
+    }
+
+    @keyframes slide-in {
+        0% {
+            transform: translateY(-10px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
+</style>
