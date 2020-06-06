5758029f56b6e0ee4ce98ea62a44d61c9eedf7bd
oguzhan.celikkaya@itelligence.com.tr <oguzhancelikkaya@gmail.com>
2020-06-06 23:46:48 +0300

    transitions v1.1

--- src/view/Movies/Movies.vue
+++ src/view/Movies/Movies.vue
@@ -5,38 +5,3 @@
         </transition>
     </div>
 </template>
-
-<style>
-    .slide-leave-active {
-        transition: opacity .3s ease;
-        opacity: 0;
-        animation: slide-out .3s ease-out forwards;
-    }
-
-    .slide-leave {
-        opacity: 1;
-        transform: translateX(0);
-    }
-
-    .slide-enter-active {
-        animation: slide-in .3s ease-out forwards;
-    }
-
-    @keyframes slide-out {
-        0% {
-            transform: translateY(0);
-        }
-        100% {
-            transform: translateY(-10px);
-        }
-    }
-
-    @keyframes slide-in {
-        0% {
-            transform: translateY(-10px);
-        }
-        100% {
-            transform: translateY(0);
-        }
-    }
-</style>
